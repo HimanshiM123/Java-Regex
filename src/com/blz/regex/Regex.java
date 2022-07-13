58d13d8d697b7f9a142b769503c4f5b3d41f2a62
Him5696 <himanshi1252@gmail.com>
2022-07-13 13:18:36 +0530

    Valid First name

--- src/com/blz/regex/Regex.java
+++ src/com/blz/regex/Regex.java
@@ -1,7 +1,17 @@
 package com.blz.regex;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
 public class Regex {
+   public void validFirstName(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to user registration program using regex");
+        System.out.println("Enter First Name :");
+        String firstName = scanner.next();
+        System.out.println(Pattern.matches("[A-Z]{1}[a-z]{2,}$", firstName));
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to User Registration Problem");
+       Regex r = new Regex();
+       r.validFirstName();
     }
 }
