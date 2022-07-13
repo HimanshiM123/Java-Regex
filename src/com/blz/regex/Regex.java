eaf1cb128ba16a7ee4b012cfa1e2a8fcd53d7e2e
Him5696 <himanshi1252@gmail.com>
2022-07-13 13:49:39 +0530

    Valid Email Id

--- src/com/blz/regex/Regex.java
+++ src/com/blz/regex/Regex.java
@@ -1,23 +1,56 @@
 package com.blz.regex;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 public class Regex {
+    final String NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+    final String LAST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+    final String EMAIL_REGEX = "abc.[a-z]{0,3}@bl.co.[a-z]{0,2}$";
     Scanner sc = new Scanner(System.in);
-   public void validFirstName(){
+
+    public void validFirstName() {
         System.out.println("Welcome to user registration program using regex");
         System.out.println("Enter First Name :");
         String firstName = sc.next();
-        System.out.println(Pattern.matches("[A-Z]{1}[a-z]{2,}$", firstName));
+        if (isValidInput(NAME_REGEX, firstName)) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+        }
     }
-    public void validLastName(){
+
+    public void validLastName() {
         System.out.println("Enter last Name :");
         String lastName = sc.next();
-        System.out.println(Pattern.matches("[A-Z]{1}[a-z]{2,}$", lastName));
+        if (isValidInput(LAST_NAME_REGEX, lastName)) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+        }
+    }
+
+    public void validEmail() {
+        System.out.println("Enter Email id:");
+        String email = sc.next();
+        if (isValidInput(EMAIL_REGEX, email)) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+        }
+    }
+
+    private static boolean isValidInput(String regex, String userInput) {
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(userInput);
+        boolean b = m.matches();
+        return b;
     }
     public static void main(String[] args) {
-       Regex r = new Regex();
-       r.validFirstName();
-       r.validLastName();
+        Regex r = new Regex();
+        r.validFirstName();
+        r.validLastName();
+        r.validEmail();
     }
 }
