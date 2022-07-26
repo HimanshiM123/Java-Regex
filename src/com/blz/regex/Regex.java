393c6ad4755d6610f73fd4945bb98bd84bd666a3
Him5696 <himanshi1252@gmail.com>
2022-07-26 22:26:48 +0530

    Mobile number regex

--- src/com/blz/regex/Regex.java
+++ src/com/blz/regex/Regex.java
@@ -5,36 +5,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Regex {
-    final String NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
-    final String LAST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
-    final String EMAIL_REGEX = "abc.[a-z]{0,3}@bl.co.[a-z]{0,2}$";
+    final String MOBILE_NUMBER_REGEX = "^(91)[0-9]{10}$";
     Scanner sc = new Scanner(System.in);
 
-    public void validFirstName() {
-        System.out.println("Welcome to user registration program using regex");
-        System.out.println("Enter First Name :");
-        String firstName = sc.next();
-        if (isValidInput(NAME_REGEX, firstName)) {
-            System.out.println("Valid");
-        } else {
-            System.out.println("Invalid");
-        }
-    }
-
-    public void validLastName() {
-        System.out.println("Enter last Name :");
-        String lastName = sc.next();
-        if (isValidInput(LAST_NAME_REGEX, lastName)) {
-            System.out.println("Valid");
-        } else {
-            System.out.println("Invalid");
-        }
-    }
-
-    public void validEmail() {
-        System.out.println("Enter Email id:");
-        String email = sc.next();
-        if (isValidInput(EMAIL_REGEX, email)) {
+    public void validMobileNum() {
+        System.out.println("Enter Mobile Number With Country Code :");
+        String mobileNum = sc.next();
+        if (isValidInput(MOBILE_NUMBER_REGEX, mobileNum)) {
             System.out.println("Valid");
         } else {
             System.out.println("Invalid");
@@ -49,8 +26,6 @@ public class Regex {
     }
     public static void main(String[] args) {
         Regex r = new Regex();
-        r.validFirstName();
-        r.validLastName();
-        r.validEmail();
+        r.validMobileNum();
     }
 }
