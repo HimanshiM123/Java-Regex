6027b45512670efa7f11ec2feda6a1900d86dab7
Him5696 <himanshi1252@gmail.com>
2022-07-26 22:45:45 +0530

    password min 8 char regex

--- src/com/blz/regex/Regex.java
+++ src/com/blz/regex/Regex.java
@@ -5,13 +5,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Regex {
-    final String MOBILE_NUMBER_REGEX = "^(91)[0-9]{10}$";
+    final String PASSWORD_REGEX = "^[A-Za-z]{8,}[$@$! %*?&]{1}$";
     Scanner sc = new Scanner(System.in);
 
-    public void validMobileNum() {
-        System.out.println("Enter Mobile Number With Country Code :");
-        String mobileNum = sc.next();
-        if (isValidInput(MOBILE_NUMBER_REGEX, mobileNum)) {
+    public void validPassword() {
+        System.out.println("Enter the password :");
+        String password = sc.next();
+        if (isValidInput(PASSWORD_REGEX, password)) {
             System.out.println("Valid");
         } else {
             System.out.println("Invalid");
@@ -26,6 +26,6 @@ public class Regex {
     }
     public static void main(String[] args) {
         Regex r = new Regex();
-        r.validMobileNum();
+        r.validPassword();
     }
 }
